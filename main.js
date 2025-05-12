fc770dbb9e5e8a46416536165fcf08a0e16fbd01
AvailableUsernameA <huc014@ucsd.edu>
2025-05-12 14:13:09 -0700

    concat dataset

--- main.js
+++ main.js
@@ -3,3 +3,16 @@
 // Male awith low HbA1c: 14, 16, 2, 12
 // Female with high HbA1c: 5, 6, 10, 4
 // Female with low HbA1c: 7, 1, 15, 8
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+async function loadData() {
+    const data = await d3.csv('loc.csv', (row) => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + 'T00:00' + row.timezone),
+      datetime: new Date(row.datetime),
+    }));
+    return data;
+}
