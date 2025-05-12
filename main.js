d5939500986535f99506dc60f5bb174ea23635eb
AvailableUsernameA <huc014@ucsd.edu>
2025-05-12 16:09:45 -0700

    button

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 // Male awith low HbA1c: 14, 2, 12
 // Female with high HbA1c: 5, 6, 10, 4
 // Female with low HbA1c: 7, 1, 15, 8
-<<<<<<< HEAD
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
@@ -75,18 +74,32 @@ function extractData(processedDatadata, cal_low, cal_high, carb_low, carb_high, 
 const input_data = await loadData('./data/001/summary001.csv');
 const processedData = processCommits(input_data);
 console.log(processedData)
-=======
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-async function loadData() {
-    const data = await d3.csv('loc.csv', (row) => ({
-      ...row,
-      line: Number(row.line), // or just +row.line
-      depth: Number(row.depth),
-      length: Number(row.length),
-      date: new Date(row.date + 'T00:00' + row.timezone),
-      datetime: new Date(row.datetime),
-    }));
-    return data;
-}
->>>>>>> fc770dbb9e5e8a46416536165fcf08a0e16fbd01
+const male_high = await loadData('./data/male_high.csv');
+const processed_male_high = processCommits(male_high);
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label id="gender-scheme">
+        Gender:
+        <select id="set-gender">
+          <option value='male'>Male</option>
+          <option value='female'>Female</option>
+          <option value='both-mf'>Both</option>
+        </select>
+    </label>`,
+);
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label id="glucose-scheme">
+        Glucose:
+        <select id="set-glucose">
+          <option value='high'>High</option>
+          <option value='low'>Low</option>
+          <option value='both-hl'>Both</option>
+        </select>
+    </label>`,
+);
