38311239752b5e56103be3cc6462b4174315b6fc
crepusidiem <wzyuwenyao@gmail.com>
2025-05-12 13:45:23 -0700

    Adding annotations

---
+++ main.js
@@ -0,0 +1,5 @@
+// 1. Divide the group into matrix of gender and whether HbA1c >= 5.7 (recommended)
+// Male with high HbA1c: 13, 11, 9
+// Male awith low HbA1c: 14, 16, 2, 12
+// Female with high HbA1c: 5, 6, 10, 4
+// Female with low HbA1c: 7, 1, 15, 8
